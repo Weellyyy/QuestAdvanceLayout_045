1dada2935ad86862f7c2a73fd6624edb77477d5e
Weellyyy <wildanprtm2304@gmail.com>
2025-10-24 15:07:46 +0700

    membuat file baru ui pertama

--- app/src/main/java/com/example/questadvancelayout_045/UIPertama.kt
+++ app/src/main/java/com/example/questadvancelayout_045/UIPertama.kt
@@ -1,2 +1,19 @@
 package com.example.questadvancelayout_045
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun ActivtasPeratama(modifier: Modifier){
+    Column (modifier= Modifier
+        .padding(top=100.dp)
+        .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){  }
+}
