f9c37c3a9995e8527c4faefddb972a319a95ac7f
Weellyyy <wildanprtm2304@gmail.com>
2025-10-24 15:21:23 +0700

    membuat function activitas pertama

--- app/src/main/java/com/example/questadvancelayout_045/UIPertama.kt
+++ app/src/main/java/com/example/questadvancelayout_045/UIPertama.kt
@@ -1,19 +1,35 @@
 package com.example.questadvancelayout_045
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
-fun ActivtasPeratama(modifier: Modifier){
+fun ActivtasPertama(modifier: Modifier){
     Column (modifier= Modifier
         .padding(top=100.dp)
         .fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally
-    ){  }
+    ){
+        Text(stringResource(R.string.prodi),
+            fontSize = 25.sp,
+            fontWeight = FontWeight.Bold
+        )
+        Text(
+            text= stringResource(R.string.univ),
+            fontSize = 18.sp
+        )
+        Spacer(modifier = Modifier.height(25.dp))
+    }
 }
