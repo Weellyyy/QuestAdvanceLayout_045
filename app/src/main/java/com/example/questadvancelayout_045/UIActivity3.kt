0481ba7b445a99da89e8c26b63f7aa3277c46031
Weellyyy <wildanprtm2304@gmail.com>
2025-10-24 18:24:06 +0700

    membuat column

--- app/src/main/java/com/example/questadvancelayout_045/UIActivity3.kt
+++ app/src/main/java/com/example/questadvancelayout_045/UIActivity3.kt
@@ -1,2 +1,23 @@
 package com.example.questadvancelayout_045
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun UIKetiga(modifier: Modifier) {
+    Column (
+        modifier = modifier
+            .fillMaxSize()
+            .background(colorResource(R.color.black))
+            .padding(horizontal = 24.dp, vertical = 40.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){  }
+
+}
